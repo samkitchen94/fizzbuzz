f6f9342cc829f31761f4e0acd9ab7e8083960098
Nicholas Barnes <n3ttl3t@gmail.com>
2019-04-25 13:29:30 +0000

    add generated number tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,78 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
+  
+=begin
   it 'returns "fizz" when its a multiple of 3' do
-    number = 0
-    until number % 3 == 0 do
+    number = 3
+    expect(fizzbuzz(number)).to eq 'fizz'
+  end
+  it 'returns "buzz" when its a multiple of 5' do
+   number = 5
+    expect(fizzbuzz(number)).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when it\'s a multiple of 3 and 5' do
+    number = 15
+    expect(fizzbuzz(number)).to eq 'fizzbuzz'
+  end
+=end
+  
+  it 'returns fizz with a random multiple  of 3' do
+    number = rand(0..100)
+    puts number
+    if number % 3 == 0 && number % 5 != 0
+      expect(fizzbuzz(number)).to eq 'fizz'
+    end
+  end
+  
+  it 'returns "buzz" with a random multiple of 5' do
+    number = rand(0..100)
+    puts number
+    if number % 5 == 0 && number % 3 != 0
+      expect(fizzbuzz(number)).to eq 'buzz'
+    end
+  end
+  
+  it 'returns "fizzbuzz" with a multiple  of 3 & 5' do
+    number = rand(0..100)
+    if number % 5 == 0 && number % 3 == 0
+      expect(fizzbuzz(number)).to eq 'fizzbuzz'
+    end
+  end
+  
+  it 'returns fizz with a random multiple of 3' do
+    number = rand(0..100)
+    puts number
+    until number % 3 == 0 && number % 5 != 0
       number = rand(0..100)
     end
     expect(fizzbuzz(number)).to eq 'fizz'
-    end
-  it 'returns "buzz" when its a multiple of 5' do
-    number = 0
-    until number % 5 == 0 do
+  end
+  
+  it 'returns buzz with a random multiple of 5' do
+    number = rand(0..100)
+    puts number
+    until number % 5 == 0 && number % 3 != 0
       number = rand(0..100)
     end
     expect(fizzbuzz(number)).to eq 'buzz'
+  end
+  
+  it 'returns fizzbuzz with a random multiple of 3 and 5' do
+    number = rand(0..100)
+    puts number
+    until number % 5 == 0 && number % 3 == 0
+      number = rand(0..100)
+    end
+    expect(fizzbuzz(number)).to eq 'fizzbuzz'
+  end
+  
+  it 'returns number with a number that isn\'t a fizz/buzz/fizzbuzz' do
+    number = rand(0..100)
+    puts number
+    until number % 5 != 0 && number % 3 != 00
+      number = rand(0..100)
+    end
+    expect(fizzbuzz(number)).to eq number
   end
 end
