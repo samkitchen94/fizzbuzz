ff2cbebac059ed7f91a08d59d0af5e5d094ffdca
Sam <samkitchen94@gmail.com>
2019-04-25 12:09:18 +0000

    working tests for multiples of 3 & 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz(number)
- 'fizz'
+ if number % 3 == 0
+   "fizz"
+ elsif number % 5 == 0
+   "buzz"
+ end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,21 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizz" when its a multiple of 3' do
+    number = 0
+    until number % 3 == 0 do
+      number = rand(0..100)
+    end
+    expect(fizzbuzz(number)).to eq 'fizz'
+    end
+  it 'returns "buzz" when its a multiple of 5' do
+    number = 0
+    until number % 5 == 0 do
+      number = rand(0..100)
+    end
+    expect(fizzbuzz(number)).to eq 'buzz'
+  end
 end
